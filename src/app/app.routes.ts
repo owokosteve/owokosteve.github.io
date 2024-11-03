6cf441be4eddf560c02efb3700502f0eadcf6455
owokosteve <stephenowoko2@gmail.com>
2024-11-03 04:54:39 +0300

    Added website pages components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { BlogComponent } from './components/blog/blog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'blog', component: BlogComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
