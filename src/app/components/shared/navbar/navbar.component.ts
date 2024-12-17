acaec6c1434ef3b0f2102d8d69d860335a3c114d
owokosteve <stephenowoko2@gmail.com>
2024-12-17 20:55:12 +0300

    Add version 2

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, ],
+  imports: [RouterLink, RouterLinkActive, CommonModule ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
+  backgroundColor: string = '#fff';
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: Event): void {
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition > 50) {
+      this.backgroundColor = 'var(--light-75)';
+    } else {
+      this.backgroundColor = '#fff';
+    }
+  }
 }
